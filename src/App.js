b6555ab169f5a95fdee27372a7d725b5da3ebb6c
pedagadi chandra sekhar <pedagadisekhar@gmail.com>
2022-12-01 19:43:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import {useState,useEffect} from "react";
+
 
 function App() {
+  
+  const [image,setImage] = useState([]);
+  const [search,setSearch]=useState("")
+  const Submithandler = (e)=>{
+    e.preventDefault()
+    axios.get(`https://api.unsplash.com/search/photos/?query=${search}&client_id=IrCRLEPT_UqNF5US2BHi3voGAc190dPJyQ837vHYCrc`)
+    .then(response=>{setImage(response.data.results);console.log(response.data)})
+     
+    .catch(err=>console.log(err))
+  
+ 
+  
+
+  
+
+  }
+
+
   return (
+    <>
+    <div className='header'>
+    <button className='bookmark'>Bookmark</button>   <h1>React photo search app</h1>
+      <input type="text" placeholder='search' value={search} onChange={(e)=>{setSearch(e.target.value)}}/>
+      <button onClick={Submithandler}>search</button>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      </div>
+      {
+        image.map((item)=>{
+          return(
+            <>
+            <div className='gallery'>
+              <div>
+            <img src={item.urls.small} alt="img"/>
+            </div>
+            </div>
+            </>
+          )
+        })
+      }
     </div>
+    </>
   );
 }
 
